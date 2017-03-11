d8c17299f6f7784c83ac53f18ebbe14048a582a1
sid0494 <bhutiya@usc.edu>
2017-03-10 19:03:52 -0800

    Completed the code for finding the tags, Still inefficient need to optimize

--- hmmdecode.py
+++ hmmdecode.py
@@ -1,8 +1,9 @@
-import sys, ast
+import sys, ast, math, timeit
 from collections import defaultdict
 
 hmm_model_path = "hmmmodel.txt"
 test_file_path = sys.argv[1]
+MIN_VALUE = -99999
 
 def load_model(hmm_path, transition, emission):
 	file = open(hmm_path)
@@ -28,6 +29,12 @@ def load_model(hmm_path, transition, emission):
 	# print vocabulary
 	# raw_input()
 
+def get_log_emission(value):
+	if value != 0.0:
+		return math.log(value)
+	else:
+		return MIN_VALUE
+
 
 def tag_data(sent, transition, emission, vocabulary):
 	
@@ -38,75 +45,56 @@ def tag_data(sent, transition, emission, vocabulary):
 	answer = ""
 	counter = 1
 	debug = 0
+	tag_list.remove("start_state")
 	for tag in tag_list:
 		# print transition["start_state"][tag], emission[tag][words[0]]
 		if vocabulary[words[0]] == 1:
-			probability[(tag, counter)] = transition["start_state"][tag] * emission[tag][words[0]]
+			probability[(tag, counter)] = transition["start_state"][tag] + emission[tag][words[0]]
 		else:
 			probability[(tag, counter)] = transition["start_state"][tag]
 		backpointer[(tag, counter)] = "start_state"
 		# print probability[(tag, counter)], tag
 		debug += 1
 	counter += 1 
-	# tag_list.remove("start_state")
+	# print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 	for w in words[1:]:
-		# w = w.lower()
-		current = 0
+
 		for tag in tag_list:
 			if vocabulary[w] == 1:
-				print "Here", w, emission[tag][w], tag
-				if w == "(" and tag == "FF":
-					for tag2 in tag_list:
-						temp = probability[(tag2, counter - 1)] * transition[tag2][tag] * emission[tag][w]
-						# print temp, tag2, transition[tag2][tag], probability[(tag2, counter - 1)]
-						if temp != 0:
-							print tag2, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-							raw_input()
-						print probability[(tag, counter)]
-						probability[(tag, counter)] = max(probability[(tag, counter)], temp)
-				else:
-					probability[(tag, counter)] = max(probability[(tag2, counter - 1)] * transition[tag2][tag] * emission[tag][w] for tag2 in tag_list)
+				probability[(tag, counter)] = max([probability[(tag2, counter - 1)] + transition[tag2][tag] + emission[tag][w] for tag2 in tag_list])
 			else:
-				# print "Here", w
-				probability[(tag, counter)] = max(probability[(tag2, counter - 1)] * transition[tag2][tag] for tag2 in tag_list)
-				# if probability[(tag, counter)] == 0:
-				# 	# print tag, counter
-				# 	# raw_input()
-			backpointer[(tag, counter)] = max(tag_list, key = lambda tag2: probability[(tag2, counter - 1)] * transition[tag2][tag])
-			print backpointer[(tag, counter)]
-				# if tag == "NP":
-				# 	print probability[(tag2, counter - 1)], transition[tag2][tag], emission[tag][w], tag, tag2, w , backpointer[(tag, counter)]
-				# 	raw_input()
-			if probability[(tag, counter)] == 0:
-				probability[(tag, counter)], tag, w, backpointer[(tag, counter)], counter
-			else:
-				current += 1
-		if current == 0:
-			print w, "Boom boom shakalaka"
-			raw_input()
-		
-		# raw_input()
+				probability[(tag, counter)] = max([probability[(tag2, counter - 1)] + transition[tag2][tag] for tag2 in tag_list])
+			
+			backpointer[(tag, counter)] = max(tag_list, key = lambda tag2: probability[(tag2, counter - 1)] + transition[tag2][tag])
+			# print probability[(tag, counter)], backpointer[(tag, counter)], tag
 		counter += 1
-		# print w, counter
+		# print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+		# raw_input()
 	
 	most_likely_tags = []
 	# print [probability[(tag, counter)] for tag in tag_list], len(words)
-	raw_input()
+	# raw_input()
+	# for tag in tag_list:
+	# 	print probability[(tag, counter - 1)], tag
 	most_likely_tags.append(max(tag_list, key = lambda tag: probability[(tag, counter - 1)]))
-	counter -= 1
+	counter = len(words)
 
 	# print words
 
 	while counter > 0:
-		print backpointer[(most_likely_tags[-1], counter)], counter
+		# print backpointer[(most_likely_tags[-1], counter)], counter
 		most_likely_tags.append(backpointer[(most_likely_tags[-1], counter)])
 		counter -= 1
 		# raw_input()
+
+
 
 	most_likely_tags = most_likely_tags[::-1][1:]
 
 	for i in range(0, len(words)):
 		answer += words[i] + "/" + most_likely_tags[i] + " "
+
+	# print answer
 
 	return answer + "\n"
 
@@ -114,19 +102,22 @@ def tag_data(sent, transition, emission, vocabulary):
 
 
 
+start = timeit.default_timer()
 
 
 
 transition = defaultdict(lambda: defaultdict(float))
-emission = defaultdict(lambda: defaultdict(float))
+emission = defaultdict(lambda: defaultdict(lambda: -99999))
 vocabulary = load_model(hmm_model_path, transition, emission)
 test_file = open(test_file_path)
 output_file = open("hmmoutput.txt", "w")
 count = 1
 for l in test_file:
-	print count, " done"
-	count += 1 
+	# print count, " done"
+	# count += 1 
 	output_file.write(tag_data(l.rstrip("\n"), transition, emission, vocabulary))
 
+stop = timeit.default_timer()
 
+print stop - start
 
--- hmmlearn.py
+++ hmmlearn.py
@@ -1,5 +1,5 @@
 # coding=UTF-8
-import math
+import sys, math
 from collections import defaultdict
 
 def parse_sent(sent, transition, emission):
@@ -9,7 +9,7 @@ def parse_sent(sent, transition, emission):
 	# current_word = tokens[0][:-3].lower()
 	current_word = tokens[0][:-3]
 	current_tag = tokens[0][-2:]
-	print tokens
+	# print tokens
 	vocabulary[current_word] = 1
 	transition["start_state"][current_tag] += 1
 
@@ -32,20 +32,25 @@ def smoothing_transition(matrix):
 
 	for tag1 in tags:
 		for tag2 in tags:
-			if tag2 == "start_state":
-				matrix[tag1][tag2] = 0
-			else:
+			if tag2 != "start_state":
 				matrix[tag1][tag2] += 1
+			# else:
+			# 	matrix[tag1][tag2] += 1
 
 
 def convert_to_probabilities(matrix):
 
-	print len(matrix)
+	# print len(matrix)
 	for key in matrix:
 		total = sum(matrix[key].values())
-		print key, len(matrix[key])
+		# print key, len(matrix[key])
 		for k in matrix[key]:
-			matrix[key][k] /= total
+			# matrix[key][k] /= total
+			# if matrix[key][k]/total != 0:
+			matrix[key][k] = math.log(matrix[key][k]/total)
+			# else:
+			# 	print key,k
+			# 	matrix[key][k] = -99999
 
 
 
@@ -54,7 +59,8 @@ transition = defaultdict(lambda: defaultdict(float))
 emission = defaultdict(lambda: defaultdict(float))
 vocabulary = {}
 
-input_file = open("catalan_corpus_train_tagged.txt")
+train_file_path = sys.argv[1]
+input_file = open(train_file_path)
 # input_file = open("test.txt")
 output_file = open("hmmmodel.txt","w")
 
